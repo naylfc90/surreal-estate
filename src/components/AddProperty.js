bc9f82525fb5fd166e3fcc016190910929593911
Nathan Horne <n.horne90@hotmail.co.uk>
2022-02-12 16:03:07 +0000

    Added form elements, form handling and CSS

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,16 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/AddProperty.css";
 
 const AddProperty = () => {
   const initialState = {
-    title: "",
+    fields: {
+      title: "",
+      city: "Liverpool",
+      type: "Flat",
+    },
   };
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
-    <div className="AddProperty">
+    <div className="add-property">
       <h1>Add Property Page</h1>
-      <form>
-        <button type="submit">Add</button>
-      </form>
+      <div className="add-property__form">
+        <form onSubmit={handleAddProperty}>
+          <label htmlFor="title">
+            Enter a Title <br />
+            <input
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+          </label>
+
+          <label htmlFor="city">
+            City <br />
+            <select
+              id="city"
+              name="city"
+              value={fields.city}
+              onChange={handleFieldChange}
+            >
+              <option value="Liverpool">Liverpool</option>
+              <option value="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+            </select>
+          </label>
+
+          <label htmlFor="type">
+            Property Type <br />
+            <select
+              id="type"
+              name="type"
+              value={fields.type}
+              onChange={handleFieldChange}
+            >
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Cottage">Cottage</option>
+              <option value="Bungalow">Bungalow</option>
+            </select>
+          </label>
+
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
   );
 };
